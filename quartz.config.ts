847af1f6392f8adb21d71fd192a0f7a092bd98f0
nRainTd <1968362785@qq.com>
2025-12-10 17:35:10 +0800

    Quartz sync: Dec 10, 2025, 5:35 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -18,7 +18,7 @@ const config: QuartzConfig = {
     locale: "zh-CN",
     baseUrl: "notes.nRainTd.cn",
     ignorePatterns: ["private", "templates", ".obsidian"],
-    defaultDateType: "created",
+    defaultDateType: "modified",
     theme: {
       fontOrigin: "googleFonts",
       cdnCaching: true,
