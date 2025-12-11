08c292e3ee53b793c228658202b703b934724437
nRainTd <1968362785@qq.com>
2025-12-11 13:46:27 +0800

    Quartz sync: Dec 11, 2025, 1:46 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -14,6 +14,7 @@ export const sharedPageComponents: SharedLayout = {
         category: "Announcements",
         categoryId: "DIC_kwDOQi-FrM4Czpff",
         lang: "zh-CN",
+        mapping: "pathname",
       },
     }),
   ],
